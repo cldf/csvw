bccfb8b82836c7aa6df3f165a494b94a40c3b39e
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-11-15 15:36:24 +0100

    release 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='csvw',
-    version='0.0.dev0',
+    version='0.1',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='',
