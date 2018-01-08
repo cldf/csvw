8928d1a59ed04bbfcd17d76ca6c163c2b093b73a
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-01-08 10:56:56 +0100

    bump major version for backwards incompatible changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='csvw',
-    version='1.1.dev0',
+    version='2.0.dev0',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='',
