04620c251db7bf92e0528ef1535c5970b70724e7
xrotwang <xrotwang@googlemail.com>
2018-04-18 11:07:07 +0200

    release 1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='csvw',
-    version='1.1.0.dev0',
+    version='1.1.0',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='',
--- src/csvw/__init__.py
+++ src/csvw/__init__.py
@@ -18,7 +18,7 @@ __all__ = [
 ]
 
 __title__ = 'csvw'
-__version__ = '1.1.0.dev0'
+__version__ = '1.1.0'
 __author__ = 'Robert Forkel'
 __license__ = 'Apache 2.0, see LICENSE'
 __copyright__ = 'Copyright (c) 2018 Robert Forkel'
