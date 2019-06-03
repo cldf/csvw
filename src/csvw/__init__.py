97fe6c779ff0ad89e2aa8b3a5411d9a0c2196450
xrotwang <xrotwang@googlemail.com>
2019-06-03 17:04:22 +0200

    release 1.5.5

--- src/csvw/__init__.py
+++ src/csvw/__init__.py
@@ -20,7 +20,7 @@ __all__ = [
 ]
 
 __title__ = 'csvw'
-__version__ = '1.5.5.dev0'
+__version__ = '1.5.5'
 __author__ = 'Robert Forkel'
 __license__ = 'Apache 2.0, see LICENSE'
 __copyright__ = 'Copyright (c) 2019 Robert Forkel'
