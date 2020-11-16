ba0da0360133c12c4c7cf7592a192326f95f52ad
xrotwang <xrotwang@googlemail.com>
2020-11-16 17:46:41 +0100

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='csvw',
-    version='1.8.1',
+    version='1.8.2.dev0',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='',
